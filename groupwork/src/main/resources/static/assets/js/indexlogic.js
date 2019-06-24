b7f5c2ddf6c7682aee437111d3c9fd7a802e343e
FungusFlowchart <542461670@qq.com>
2019-06-24 16:21:45 +0800

    add front logic

--- groupwork/src/main/resources/static/assets/js/indexlogic.js
+++ groupwork/src/main/resources/static/assets/js/indexlogic.js
@@ -30,3 +30,29 @@ function addManager() {
         window.reload();
 })
 }
+
+function editManager() {
+    var userid = document.getElementById("e-admin-id").value;
+    var password = document.getElementById("e-admin-pass").value;
+    var name = document.getElementById("e-admin-name").value;
+    var tel = document.getElementById("e-admin-contact").value;
+    axios.post('/editManager',{
+        userid:userid,
+        password:password,
+        name:name,
+        tel:tel
+    }).then(response =>{
+        window.reload();
+})
+}
+
+function deleteManager() {
+    var userid = document.getElementById("del-admin-id").value;
+    
+    axios.post('/deleteManager',{
+        userid:userid
+    }).then(response =>{
+        window.reload();
+})
+}
+
