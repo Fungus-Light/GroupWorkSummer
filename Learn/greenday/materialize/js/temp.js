64c35438c503471bfc7010668a44a695ec405afa
greenday12138 <31298750+greenday12138@users.noreply.github.com>
2019-06-24 20:55:36 +0800

    finish quarter of JS

--- Learn/greenday/materialize/js/temp.js
+++ Learn/greenday/materialize/js/temp.js
@@ -1,9 +1,8 @@
-var length=["52","50"];
+var leng=[520,60,54,98,50];
 var temp={
     hdhh:'40',
     te:'60 '
 }
-length.push("454");
-length.unshift("djdj");
-length.splice(2,1,"Lemon","Kiwi");
-window.alert(length);
+var d=new Date();
+window.alert((d.getMonth()).toDateString());
+//window.alert(leng);
