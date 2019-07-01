4619f069c56849420dc06f805611e387575e3a17
赵子淇 <zh450421765@qq.com>
2019-07-01 10:14:36 +0800

    修改后，学生和管理员都可以只看见对应学院的题目了

--- groupwork/src/main/java/com/minqing/demo/entity/TopicRepository.java
+++ groupwork/src/main/java/com/minqing/demo/entity/TopicRepository.java
@@ -14,7 +14,7 @@ public interface TopicRepository extends JpaRepository<Topic,Integer> {
     List<Topic> findByUserid(String userid);
     List<Topic> findByState(Integer state);
     @Modifying
-    @Query("select topicid from student,teacher,topic where student.academic=teacher.academic " +
-            "and teacher.userid=topic.userid and student.academic=?1")
+    @Query(value = "select topicid from Student,Teacher,Topic where student.academic=teacher.academic " +
+            "and teacher.userid=topic.userid and student.academic=?1",nativeQuery = true)
     List findTopicidByAcademic(String academic);
 }
